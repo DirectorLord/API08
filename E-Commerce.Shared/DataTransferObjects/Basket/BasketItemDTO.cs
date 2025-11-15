b59eeb32eab06459f513a64b1911635e51b12f1f
Director Lord <thelord161203@gmail.com>
2025-11-15 13:30:12 +0200

    finished the demo

--- E-Commerce.Shared/DataTransferObjects/Basket/BasketItemDTO.cs
+++ E-Commerce.Shared/DataTransferObjects/Basket/BasketItemDTO.cs
@@ -3,8 +3,7 @@
 public class BasketItemDTO
 {
     public int Id { get; set; }
-    public string Name { get; init; }
-    public string Description { get; init; }
+    public string ProductName { get; init; }
     public string PictureUrl { get; init; }
     public decimal Price { get; init; }
     public int Quantity { get; set; }
